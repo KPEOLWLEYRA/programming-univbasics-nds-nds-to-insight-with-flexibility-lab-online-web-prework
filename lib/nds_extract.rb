9d797ff4e1faf1c446d44ba51424611d769a1e70
Kelly Powers <kpowers.nj@gmail.com>
2020-01-06 21:00:42 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -106,7 +106,7 @@ def movies_with_directors_set(source)
   index = 0 
   new_array = []
   while index < source.length do 
-    movie_info = movies_with_director_name(source[index], source[index][:movies])
+    movie_info = movie_with_director_name(source[index], source[index][:movies])
     new_array<< movie_info 
     index +=1 
   end
